43639f80b96cbca1388b851ee7f89f77a8415c48
dustin-jw <dustin@heysparkbox.com>
2023-10-02 08:39:27 -0600

    feat: replace boilerplate CSS with simpler styles
    - remove unused classes/redundant wrapper divs
    - limit pagination to show fewer pages
    - apply minimal styling to 404 page
    - remove unused pagination classes
    - make default element styles less specific
    - replace normalize with more modern CSS reset
    - update element styles
    - update focus styles and colors
    - update width limiter
    - add skip-to-content link styles
    - push footer to bottom of page
    - give footer some space
    - add basic nav styles
    - support menu interactivity
    - remove/update outdated CSS info

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
 import { toggleNoJS } from './helpers/toggle-no-js';
+import { initializeDisclosureButtonHandler } from './helpers/toggle-disclosure';
 
 toggleNoJS();
+initializeDisclosureButtonHandler('.cmp-main-nav__toggler');
