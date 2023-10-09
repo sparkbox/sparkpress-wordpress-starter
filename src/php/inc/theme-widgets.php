4108f617814570012a6c0ea0dcac698e8678f171
rise-erpelding <54716846+rise-erpelding@users.noreply.github.com>
2023-10-09 06:40:28 -1000

    feat: support multiple sidebars (#82)
    * feat: register sidebar for aside
    
    * feat: basic primary sidebar styles
    
    * refactor: adjust widget registration and styles
    
    - register and add widget areas to context in same file
    - rename widgets/widget areas
    - remove widget area titles
    - adjust css for sidebar widget area

--- src/php/inc/theme-widgets.php
+++ src/php/inc/theme-widgets.php
@@ -6,19 +6,40 @@
  */
 
 /**
- * Register widget area.
+ * Register widgetized areas.
  */
 function sparkpress_theme_widgets_init() {
 	register_sidebar(
 		array(
-			'name'          => 'Footer',
-			'id'            => 'footer-area',
-			'description'   => 'Sidebar for a the footer area.',
+			'name'          => 'Primary Sidebar',
+			'id'            => 'primary-sidebar',
+			'description'   => 'Widget for the sidebar area.',
 			'before_widget' => '<div>',
 			'after_widget'  => '</div>',
-			'before_title'  => '<h3>',
-			'after_title'   => '</h3>',
+		)
+	);
+
+	register_sidebar(
+		array(
+			'name'          => 'Footer',
+			'id'            => 'footer-area',
+			'description'   => 'Widget for the footer area.',
+			'before_widget' => '<div>',
+			'after_widget'  => '</div>',
 		)
 	);
 }
 add_action( 'widgets_init', 'sparkpress_theme_widgets_init' );
+
+/**
+ * Adds sidebar and footer widgets to the Timber context.
+ *
+ * @param array $context The Timber context array.
+ * @return array The updated Timber context array with sidebar and footer widgets.
+ */
+function sparkpress_add_widgets_to_context( $context ) {
+	$context['sidebar_widget'] = Timber\Timber::get_widgets( 'primary-sidebar' );
+	$context['footer_widget'] = Timber\Timber::get_widgets( 'footer-area' );
+	return $context;
+}
+add_filter( 'timber/context', 'sparkpress_add_widgets_to_context' );
